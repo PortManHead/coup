52a232e8c2b6ef2d3f2215806d714675c101b585
porter <mr.porteiro.head@gmail.com>
2016-01-24 09:41:43 -0800

    Add sensor type to .coup config file

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 SensorLog.py SETUP:
 You must have a ~/.coup file with a dictionary mapping from your sensor number
-to the GPIO pin used for it.
+to a (sensorType, gpioPin) tuple used for it.
 
 For example, here is mine:
 
-{1:23, 2:24, 3:25, 4:8} # Sensor Number to GPIO pin dictionary
+{1:(11,23), 2:(11,24), 3:(11,25), 4:(11,8)}
+This shows that sensor #1 is a #11 type sensor, and it is attached to GPIO pin #23
 
 This python program must be run with root priveledges.
 
--- sensorLog.py
+++ sensorLog.py
@@ -4,13 +4,12 @@ import time
 import Adafruit_DHT
 import config
 
-sensorPins = config.sensorPins
-#sensorPins = {1:23, 2:24, 3:25, 4:8}
+sensors = config.sensorPins
 
 
 def Main():
     print TimeMessage()
-    for sensorNum in sensorPins.keys():
+    for sensorNum in sensors.keys():
         print SensorMessage(sensorNum)
 
 def TimeMessage():
@@ -18,18 +17,17 @@ def TimeMessage():
 
 
 def SensorMessage(sensorNum):
-    temp, humidity = GetTempHumidity(sensorPins[sensorNum])
+    temp, humidity = GetTempHumidity(sensors[sensorNum])
     msg = "Sensor #%d: %.0f deg F, %.0f%% humidity" % (sensorNum, temp, humidity)
     return msg
 
 
-def GetTempHumidity(pin, sensorNum='11'):
-# Parse command line parameters.
-    sensor_args = { '11': Adafruit_DHT.DHT11,
-		    '22': Adafruit_DHT.DHT22,
-		    '2302': Adafruit_DHT.AM2302 }
+def GetTempHumidity((sensorType, pin)):
+    sensor_args = { 11: Adafruit_DHT.DHT11,
+		    22: Adafruit_DHT.DHT22,
+		    2302: Adafruit_DHT.AM2302 }
 
-    sensor = sensor_args[sensorNum]
+    sensor = sensor_args[sensorType]
 
     humidity, tempC = Adafruit_DHT.read_retry(sensor, pin)
     tempF = CTempToF(tempC)
@@ -40,7 +38,6 @@ def GetTempHumidity(pin, sensorNum='11'):
 def CTempToF(c):
     f = (c * 1.8) + 32
     return f
-
 
 
 
